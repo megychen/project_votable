00113204a7bde5c7aeb81b40d4a1bfcd16b3c9ed
megy <megy@megys-MacBook-Pro.local>
2017-04-26 18:00:19 +0800

    implement project model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Project.create!(name: "Project one", description: "Description one")
+Project.create!(name: "Project two", description: "Description two")
+Project.create!(name: "Project three", description: "Description three")
+Project.create!(name: "Project four", description: "Description four")
+Project.create!(name: "Project five", description: "Description five")
